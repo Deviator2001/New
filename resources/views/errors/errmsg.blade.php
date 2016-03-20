5e05228fdbc397fa43387ce90661e579832ff6b7
deviator2001 <deviator2001@mail.ru>
2016-03-20 19:55:31 +0200

    utf-8

--- resources/views/errors/errmsg.blade.php
+++ resources/views/errors/errmsg.blade.php
@@ -1,13 +1,19 @@
 <?php
-?>
+/**
+ * Created by PhpStorm.
+ * User: Dmitriy Pivovarov aka AngryDeer http://studioweb.pro
+ * Date: 25.01.16
+ * Time: 4:16
+ */?>
+
 @if ($errors->any())
     <div class="alert alert-danger alert-block">
         {{-- <button type="button" class="close" data-dismiss="alert"><i class="fa fa-minus-square"></i></button> --}}
-        <strong>Ошибка</strong>
+        <strong>РћС€РёР±РєР°</strong>
         @if ($message = $errors->first(0, ':message'))
             {{ $message }}
         @else
-            Пожалуйста проверьте правильность заполнения формы
+            РџРѕР¶Р°Р»СѓР№СЃС‚Р° РїСЂРѕРІРµСЂСЊС‚Рµ РїСЂР°РІРёР»СЊРЅРѕСЃС‚СЊ Р·Р°РїРѕР»РЅРµРЅРёСЏ С„РѕСЂРјС‹
         @endif
     </div>
 @endif
